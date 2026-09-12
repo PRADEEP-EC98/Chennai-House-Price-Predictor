a26898751eaaa696739c793c809909910bd97ec3
PRADEEP-EC98 <pradeepkumarcall2225@gmail.com>
2026-09-12 23:47:51 +0530

    update app

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ with col_results:
                     formatted_price = f"₹{price_lakh / 100000:,.2f} Thousand Crore"
 
                 # Render output directly in UI
-                st.success(f"**Success:**")
+                #st.success(f"**Success:**")
                 st.metric(label="Predicted Value", value=formatted_price)
 
             except Exception as err:
